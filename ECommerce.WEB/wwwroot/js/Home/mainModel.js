9aead1d5599ab74178d7b2863fe583420d199302
jmarcos15vnz <47579087+jmarcos15vnz@users.noreply.github.com>
2020-05-26 22:36:06 -0300

    Update Files layout

---
+++ ECommerce.WEB/wwwroot/js/Home/mainModel.js
@@ -0,0 +1,35 @@
+﻿
+function MainModel(model) {
+    var self = this;
+
+    function Authentication(email, pass) {
+        email = self.txtEmail;
+        pass = self.txtPassword;
+    }
+
+    self.txtEmail = ko.observable();
+    self.txtPassword = ko.observable();
+
+    self.OnClickLogin = function () {
+        self.txtPassword = ko.observable("joao")
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
